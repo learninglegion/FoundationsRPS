8a1426a5dd49f846bef5bd284f195ffe910eb785
B. Beckwith <33493824+learninglegion@users.noreply.github.com>
2025-04-17 08:22:22 -0400

    Generate choice logic

--- RPS.js
+++ RPS.js
@@ -1,1 +1,11 @@
 console.log("Hello, World!");
+
+// Generate random number to simulate cpu choice
+// Maybe take the first number in stroing that is not
+// a 0 instead of rerolling until * 10 is not 0?
+function getComputerChoice() {
+    let choice = Math.random() * 10
+    console.log(choice);
+}
+
+getComputerChoice();

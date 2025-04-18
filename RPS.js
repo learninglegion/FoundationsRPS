75dab86972288c4e8ffbe9a883e3a2376d9873c7
B. Beckwith <33493824+learninglegion@users.noreply.github.com>
2025-04-18 09:10:02 -0400

    Create choice logic
    Have a few ideas on how to generate choice. Using random I
    can generate a digit from 0-9, but have to find a way to
    remove results of 0 to create a perfect 33.3 repeating chance.
    Went with while loop iterating over the random string after
    the decimal.

--- RPS.js
+++ RPS.js
@@ -3,9 +3,28 @@ console.log("Hello, World!");
 // Generate random number to simulate cpu choice
 // Maybe take the first number in stroing that is not
 // a 0 instead of rerolling until * 10 is not 0?
+// function getComputerChoice() {
+//     let randNum = String(Math.random());
+//     choice = +randNum.charAt(2);
+//     if (choice === 0) {
+//         console.log("got to if");
+//         getComputerChoice();
+//     } else {
+//     return choice;
+//     }
+// }
+
+
 function getComputerChoice() {
-    let choice = Math.random() * 10
-    console.log(choice);
+    const randNum = String(Math.random());
+    let choice = 0;
+    let i = 2;
+    while (choice == 0 ?? i < randNum.length) {
+        console.log(+randNum.charAt(i));
+        choice = +randNum.charAt(i);
+        i++;
+    }
+    return choice;
 }
 
-getComputerChoice();
+console.log(getComputerChoice());
